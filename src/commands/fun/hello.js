7d25ff673de343f91e258f3111432e3d5fb64fea
Tyler Nielson <tylernielson1@users.noreply.github.com>
2026-08-19 13:18:48 -0500

    linter fix

--- src/commands/fun/hello.js
+++ src/commands/fun/hello.js
@@ -9,6 +9,6 @@ module.exports = {
 	async execute(interaction) {
 		const name = interaction.options.getString('name');
 		if (!name) await interaction.reply('Hello, darlings. The witching hour has begun.');
-		else await interaction.reply(`Ahem… hello ${name}. I\’ve been summoned.`);
+		else await interaction.reply(`Ahem… hello ${name}. I've been summoned.`);
 	},
 };
