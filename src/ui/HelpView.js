d5a4e7da1e37b1dba2161b9aeddcb9c5f5f8cf2b
Tyler Nielson <tylernielson1@users.noreply.github.com>
2026-08-20 16:21:00 -0500

    linter run

--- src/ui/HelpView.js
+++ src/ui/HelpView.js
@@ -1,98 +1,98 @@
-const { 
-    ActionRowBuilder,
+const {
+	ActionRowBuilder,
 	ButtonBuilder,
 	ButtonStyle,
-    EmbedBuilder 
+	EmbedBuilder,
 } = require('discord.js');
 
 class HelpView {
-    render(command, commandIndex, totalCommands) {
-        const embed = new EmbedBuilder()
-            .setColor(0x4F46E5)
-            .setTitle('Command Help');
+	render(command, commandIndex, totalCommands) {
+		const embed = new EmbedBuilder()
+			.setColor(0x4F46E5)
+			.setTitle('Command Help');
 
-        const parameters = this.buildCommandOptionsString(command.options);
+		const parameters = this.buildCommandOptionsString(command.options);
 
-        embed.setDescription(
-            [
-                `Command Name: **/${command.name}**`,
-                `Command Description: ${command.description}`,
-                parameters ? `\nParameters:\n${parameters}` : null,
-            ].filter(Boolean).join('\n')
-        );
+		embed.setDescription(
+			[
+				`Command Name: **/${command.name}**`,
+				`Command Description: ${command.description}`,
+				parameters ? `\nParameters:\n${parameters}` : null,
+			].filter(Boolean).join('\n'),
+		);
 
-        const components = [];
+		const components = [];
 
-        if (totalCommands > 1) {
-            components.push(
-                this.buildPaginationButtons(
-                    commandIndex,
-                    totalCommands,
-                ),
-            );
-        }
+		if (totalCommands > 1) {
+			components.push(
+				this.buildPaginationButtons(
+					commandIndex,
+					totalCommands,
+				),
+			);
+		}
 
-        components.push(this.buildCloseButton())
+		components.push(this.buildCloseButton());
 
-        return {
-            embeds: [embed],
-            components,
-        };
-    }
+		return {
+			embeds: [embed],
+			components,
+		};
+	}
 
-    buildPaginationButtons(commandIndex, totalCommands) {
-        return new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setCustomId(`help_prev:${commandIndex}`)
-                    .setEmoji('⬅️')
-                    .setStyle(ButtonStyle.Secondary)
-                    .setDisabled(commandIndex === 0),
+	buildPaginationButtons(commandIndex, totalCommands) {
+		return new ActionRowBuilder()
+			.addComponents(
+				new ButtonBuilder()
+					.setCustomId(`help_prev:${commandIndex}`)
+					.setEmoji('⬅️')
+					.setStyle(ButtonStyle.Secondary)
+					.setDisabled(commandIndex === 0),
 
-                new ButtonBuilder()
-                    .setCustomId(`help_next:${commandIndex}`)
-                    .setEmoji('➡️')
-                    .setStyle(ButtonStyle.Secondary)
-                    .setDisabled(commandIndex >= totalCommands - 1),
-            );
-    }
+				new ButtonBuilder()
+					.setCustomId(`help_next:${commandIndex}`)
+					.setEmoji('➡️')
+					.setStyle(ButtonStyle.Secondary)
+					.setDisabled(commandIndex >= totalCommands - 1),
+			);
+	}
 
-    buildCloseButton() {
-        return new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setCustomId('help_close')
-                    .setLabel('Close')
-                    .setStyle(ButtonStyle.Danger),
-            );
-    }
+	buildCloseButton() {
+		return new ActionRowBuilder()
+			.addComponents(
+				new ButtonBuilder()
+					.setCustomId('help_close')
+					.setLabel('Close')
+					.setStyle(ButtonStyle.Danger),
+			);
+	}
 
-    buildCommandOptionsString(options) {
-        if (!options?.length) {
-            return null;
-        }
+	buildCommandOptionsString(options) {
+		if (!options?.length) {
+			return null;
+		}
 
-        return options.map((option) => {
-            const allowedValues = this.buildOptionChoicesString(option.choices);
+		return options.map((option) => {
+			const allowedValues = this.buildOptionChoicesString(option.choices);
 
-            return [
-                `Parameter Name: ${option.name}`,
-                `Parameter Description: ${option.description}`,
-                `Required: ${option.required ? 'Yes' : 'No'}`,
-                allowedValues ? `Allowed values: ${allowedValues}` : null,
-            ].filter(Boolean).join('\n')
-        }).join('\n\n');
-    }
+			return [
+				`Parameter Name: ${option.name}`,
+				`Parameter Description: ${option.description}`,
+				`Required: ${option.required ? 'Yes' : 'No'}`,
+				allowedValues ? `Allowed values: ${allowedValues}` : null,
+			].filter(Boolean).join('\n');
+		}).join('\n\n');
+	}
 
-    buildOptionChoicesString(choices) {
-        if (!choices?.length) {
-            return null;
-        }
+	buildOptionChoicesString(choices) {
+		if (!choices?.length) {
+			return null;
+		}
 
-        return choices.map((choice) => {
-            return `${choice.name}`;
-        }).join(', ');
-    }
+		return choices.map((choice) => {
+			return `${choice.name}`;
+		}).join(', ');
+	}
 }
 
 module.exports = new HelpView();
